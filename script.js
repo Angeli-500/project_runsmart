363da831fde84b12d0f189f5d3a9a304fcf9fd8a
Angeli-500 <124200779+Angeli-500@users.noreply.github.com>
2023-06-03 16:21:42 +0300

    added catalog

--- script.js
+++ script.js
@@ -16,10 +16,31 @@ var slider = tns({
     }
   });
 
-  document.querySelector('.next').onclick = function () {
-    slider.goTo('next');
-  };
+document.querySelector('.next').onclick = function () {
+  slider.goTo('next');
+};
 
-  document.querySelector('.prev').onclick = function () {
-    slider.goTo('prev');
-  };
+document.querySelector('.prev').onclick = function () {
+  slider.goTo('prev');
+};
+
+  
+
+$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+  $(this)
+    .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+    .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+});
+
+ 
+
+function toggleSlide(item) {
+  $(item).each(function(i) {
+    $(this).on('click', function(e) {
+      e.preventDefault();
+      $('.catalog-item__flipper').eq(i).toggleClass('catalog-item__flipper_active');
+    }) 
+  });
+};
+toggleSlide('.catalog-item__more');
+toggleSlide('.catalog-item__back');
